8819c4303440d1a9e33981b78bd9bff7f84494b2
Alberto <alberto.ghiotto@hotmail.it>
2019-10-14 10:20:30 +0200

    Update

--- NaiveBayesClassifier.m
+++ NaiveBayesClassifier.m
@@ -12,29 +12,42 @@ classLabel = dataset(1:data,features);        %class labels (ground truth)
 %% Check that the number of columns of the second matrix equals 
 %  the number d of columns of the first matrix – 1
 
-[trSetRow, trSetCols] = size(trainSet);
-[testSetRow, testSetCols] = size(testSet);
+[trSetRows, trSetCols] = size(trainSet);
+[testSetRows, testSetCols] = size(testSet);
 
 if( (trSetCols -1) ~= testSetCols)
     disp("Error! The train and test set are not correctly dimensioned");
-
+end
 %% Check that no entry in any of the two data sets is <1
-for row = 1:trSetRow
+for row = 1:trSetRows
     for col = 1:trSetCols
-        if (trainSet(row,col) < 1)
-            disp("Error! The values in the train test are incosistent");
+        if (trainSet(row,col) < 0)
+            disp(['Error! The values in the train set in position (', num2str(row),',',num2str(col),') are inconsistent']);
         end
     end
 end
 
-for row = 1:testSetRow
-    for col = 1:testSetRow
-        if (testSetRow(row,col) < 1)
-            disp("Error! The values in the test test are incosistent");
+for row = 1:testSetRows
+    for col = 1:testSetCols
+        if (testSet(row,col) < 0)
+            disp(['Error! The values in the test set in position (', num2str(row),',',num2str(col),') are inconsistent']);
         end
     end
 end
 
+%% Train a Naive Bayes classifier on the training set 
+%  (first input argument), using its last column as the target
 
+% First I need to evaluate the class vector, count how many classes 
+% there are and compute their probability
+
+[n_classes, t] = size(classLabel);
+
+classes = classLabel(1,1);  % Initially set classes as only the first element of the class vector
+for row = 1:n_classes
+   if( classLabel(row,1) ~= classes)
+        classes = [classes; classLabel(row,1)];  %append each new element to the class vector
+   end
+end
 
 
