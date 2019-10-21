703bb539211d38422d3ba65cd64eecc3d2aacc9f
Alberto <alberto.ghiotto@hotmail.it>
2019-10-21 18:52:47 +0200

    Cleaned repo

--- Assignment 1 - Naive Bayes Classifier/script.m
+++ Assignment 1 - Naive Bayes Classifier/script.m
@@ -1,17 +1,17 @@
 clear; clc;
 %% Load dataset and divide in train and test set and class labels
+%Line to work with weather dataset
 preDataset = load('weather2.data');       %Load the dataset
 
-% preDataset = load('mushroom.csv');
-
-% preDataset = [preDataset(:, 2:end) preDataset(:,1)];
+%Line to work with mushroom dataset
+% preDataset = load('mushroom.csv');  preDataset = [preDataset(:, 2:end) preDataset(:,1)];
 
 % randomize dataset entries for consistency in division between train and
 % test set
  dataset = preDataset(randperm(size(preDataset, 1)), :); 
 [data, cols] = size(preDataset);
 features = cols -1 ; % take out the class labels from the features
-n =10;
+n =10;  %Set to ~10 for weather and to ~6600 for mushroom
 %Divide dataset in:
 trainSet = dataset(1:n, 1:features+1);          %train set
 testSet = dataset(n+1:data, 1:features);        %test set
