d10ba4ee79093951444b97b990054355de878f19
Alberto <alberto.ghiotto@hotmail.it>
2019-11-24 22:48:16 +0100

    Reorganized folder of project 3. Updated readme 1

--- Assignment 1 - Naive Bayes Classifier/README.md
+++ Assignment 1 - Naive Bayes Classifier/README.md
@@ -1,3 +1,3 @@
 # The Naive Bayes Classifier
-The aim of this project was to implement a Naive Bayes
+The aim of this project is to implement a Naive Bayes
 classifier
--- Assignment 3 - kNN classifier/script.m
+++ Assignment 3 - kNN classifier/script.m
@@ -39,7 +39,7 @@ end
 % k = 1:201;
 % for y = 1:10
 %     figure;
-%     name = 'result_k1-201_digit' + string(y) + '.mat';
+%     name = 'result_files/result_k1-201_digit' + string(y) + '.mat';
 %     load(name);
 % %     bar(result(1:2:end,2),0.8);
 %     plot(k,result(1:2:end,2)*100);
@@ -57,7 +57,7 @@ digits = [ 1 2 3 4 5 6 7 8 9 0];
 k = 1:2:201;
 for y = 1:10
     figure;
-    name = 'result_k1-201_digit' + string(y) + '.mat';
+    name = 'result_files/result_k1-201_digit' + string(y) + '.mat';
     load(name);
     bar(k,(1-result(1:2:end,2))*100, 0.7);
     title(["Accuracy trend with digit ", string(digits(y)) ," vs the remaining nine"]);
@@ -70,7 +70,7 @@ end
 
 %% Print accuracy trend for test 2 - it needs the files made from test_2.m
 figure;
-load('result_several_k1.mat');
+load('result_files/result_several_k1.mat');
 bar(k,(1-result(1:2:end,2))*100);
 title(["Accuracy trend (%) for several values of k"]);
 xlabel("k values ");
