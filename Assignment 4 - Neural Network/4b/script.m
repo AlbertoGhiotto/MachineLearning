3af26fd2f38dd1491a37c8c21e73cff1c362ad6b
Alberto <alberto.ghiotto@hotmail.it>
2019-12-07 19:29:04 +0100

    Updated plot

--- Assignment 4 - Neural Network/4b/script.m
+++ Assignment 4 - Neural Network/4b/script.m
@@ -6,20 +6,18 @@ clear; close all; clc;
 
 %% Task 1: Get The Data
 % Take only two classes
-classes = [2 8];
+classes = [10 1 5 ];
 % Load train set
 [trainSet, labels]  = loadMNIST(0, classes);
 % trainSet = [data, labels];
 
-% Divide the dataset 
+% Divide the dataset
 n = 10;
 trainSet = trainSet(1:floor(end/n),1:end);
+labels = labels(1:floor(end/n),1);
 trainSet = trainSet';
-% Normalize label vector  (not necessary)
-% labels = normalizeLabel(labels);
-
-
-nh = 10;
+%%
+nh = 3;
 % Train autoencoder on the new, reduced training set
 myAutoencoder = trainAutoencoder(trainSet,nh);
 % Encode the different classes using the encoder obtained
@@ -28,9 +26,5 @@ myEncodedData = encode(myAutoencoder,trainSet);
 % Denormalize data for plotting
 myEncodedData = myEncodedData';
 
-n = size(myEncodedData,1);
-t = [1 2]';
+t = labels;
 plotcl(myEncodedData, t);
-
-
-
